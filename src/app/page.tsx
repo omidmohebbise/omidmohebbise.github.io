5208f82db1f179e8e75b0b0a1931f655ced50f3a
o.mohebbi <omidmohebbise@gmail.com>
2024-08-31 16:26:56 +0200

    add bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>
-      Omid Mohebbi
+    <main className="container-fluid">
+     <div className="row">
+        <div className="col"> Hello </div>
+        <div className="col"> World </div>
+
      </div>
     </main>
   );
