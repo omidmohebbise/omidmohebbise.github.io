71cd788393b5b2034f50427ca5bb35c69740d5cd
o.mohebbi <omidmohebbise@gmail.com>
2024-09-02 17:32:16 +0200

    add java and aws routes

---
+++ src/app/aws/page.tsx
@@ -0,0 +1,5 @@
+export default function AWS() {
+    return <div>
+       AWS Home
+    </div>
+}
---
+++ src/app/java/jdk21/page.tsx
@@ -0,0 +1,5 @@
+export default function Jdk21() {
+    return <div>
+        Java, JDK 21
+    </div>
+}
