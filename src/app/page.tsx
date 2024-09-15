22d41881718629f044b21d179cf79a529a3c12e9
o.mohebbi <omidmohebbise@gmail.com>
2024-09-15 12:35:29 +0200

    update work experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Experiences from "@/components/experience/work-experiences";
 import Skills from "@/components/Skills";
 
 export default function Home() {
@@ -13,116 +14,14 @@ export default function Home() {
 
       <Skills/>
 
-      <section id="experience" className="bg-light">
-        <div className="container">
-          <h2 className="section-title">Experience</h2>
-
-          <div className="row">
-            <div className="col-md-12">
-              <div className="experience-item">
-                <h3>Senior Software Engineer</h3>
-                <p><strong>NAMO BPM AS (Oslo, Norway) / Remote Working From Isfahan, Iran</strong></p>
-                <p><em>July 2021 - August 2023</em></p>
-                <ul>
-                  <li>Designed a new microservice-based architecture for the company's NAMO products, increasing reliability and scalability by 40% with a reduced infrastructure cost of $1000+ monthly.</li>
-                  <li>Contributed as a tech leader in initiating, developing, and delivering 7+ Java, Kotlin, React.js, and Next.js applications from scratch.</li>
-                  <li>Implemented CI/CD based on GitLab, GitLabRunner, and Azure DevOps to streamline the development and deployment processes.</li>
-                  <li>Worked on microservices and managed and monitored the AWS cloud infrastructure.</li>
-                  <li>Created unit, integration, and end-to-end tests, using automated testing and TDD methods, to achieve a code coverage rate of more than 70%.</li>
-                  <li>Integrated Stripe and Vipps for seamless payment processing and implemented a secure accountant module in the app.</li>
-                  <li>Developed consistent design and development standards, aimed at enhancing product development efficiency by 30%.</li>
-                </ul>
-                <p><strong>Stack:</strong> Java 17, Spring & Hibernate, RestAPIs, AWS, NodeJS, Express, Docker, JS/Typescript, React.js, Next.js, PostgreSQL, MongoDB, Kafka, Redis, Prometheus, Grafana, GitLab CI/CD, Azure DevOps</p>
-              </div>
-            </div>
-          </div>
+      <Experiences/>
 
 
 
 
 
-
-          <div className="row">
-            <div className="col-md-12">
-              <div className="experience-item">
-                <h3>Senior Software Engineer</h3>
-                <p><strong>NAMO BPM AS (Oslo, Norway) / Remote Working From Isfahan, Iran</strong></p>
-                <p><em>July 2021 - August 2023</em></p>
-                <ul>
-                  <li>Designed a new microservice-based architecture for the company's NAMO products, increasing reliability and scalability by 40% with a reduced infrastructure cost of $1000+ monthly.</li>
-                  <li>Contributed as a tech leader in initiating, developing, and delivering 7+ Java, Kotlin, React.js, and Next.js applications from scratch.</li>
-                  <li>Implemented CI/CD based on GitLab, GitLabRunner, and Azure DevOps to streamline the development and deployment processes.</li>
-                  <li>Worked on microservices and managed and monitored the AWS cloud infrastructure.</li>
-                  <li>Created unit, integration, and end-to-end tests, using automated testing and TDD methods, to achieve a code coverage rate of more than 70%.</li>
-                  <li>Integrated Stripe and Vipps for seamless payment processing and implemented a secure accountant module in the app.</li>
-                  <li>Developed consistent design and development standards, aimed at enhancing product development efficiency by 30%.</li>
-                </ul>
-                <p><strong>Stack:</strong> Java 17, Spring & Hibernate, RestAPIs, AWS, NodeJS, Express, Docker, JS/Typescript, React.js, Next.js, PostgreSQL, MongoDB, Kafka, Redis, Prometheus, Grafana, GitLab CI/CD, Azure DevOps</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-12">
-              <div className="experience-item">
-                <h3>Software Engineer</h3>
-                <p><strong>IRISA (Isfahan, Iran)</strong></p>
-                <ul>
-                  <li>Designed and executed a BPM and Web foundation to facilitate the development of business processes.</li>
-                  <li>Served as a liaison between the technical, functional, and testing teams.</li>
-                  <li>Estimated and performed feasibility studies while preparing and delivering new requirements.</li>
-                  <li>Collaborated alongside 60+ programmers to enhance testing procedures to reduce production time and elevate coding standards.</li>
-                  <li>Leveraged management and innovative engineering strategies to maximize the team's potential and individual growth.</li>
-                  <li>Improved applications' efficiency, data quality, scope, operability, and flexibility by 30%.</li>
-                  <li>Delivered over 20% of high-quality business modules and subsystems to meet client satisfaction.</li>
-                </ul>
-                <p><strong>Stack:</strong> Java 6-8 & J2EE, JSF2, Servlet, Oracle DB, ADF, BPMS, WebLogic, OAM, OID, OEM, ELK</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-12">
-              <div className="experience-item">
-                <h3>Senior Backend Developer</h3>
-                <p><strong>RAYADIAR System (Isfahan, Iran)</strong></p>
-                <p><em>July 2019 - Jun 2021</em></p>
-                <ul>
-                  <li>Designed and constructed 3+ innovative Java Spring Boot modules including 100+ RestAPIs utilizing microservice architecture.</li>
-                  <li>Refactored the legacy application into reusable architecture by gradually migrating to microservice with Rest, JAX-RS, and JAX-WS APIs and micro frontend web applications.</li>
-                  <li>Monitored software synchronization with the application and system architecture.</li>
-                  <li>Enhanced code maintenance by 30% through the implementation of custom standards and Clean Code principles.</li>
-                  <li>Boosted solution security by 20% through the enhancement of infrastructure and application security measures.</li>
-                </ul>
-                <p><strong>Stack:</strong> Java 11, Spring & Hibernate, RestAPIs, PostgreSQL, MongoDB, Docker & Swarm, AWS, gRPC, React, Jenkins, ELK, Kafka, Redis, Prometheus, Grafana</p>
-              </div>
-
-              <div className="experience-item">
-                <h3>Full Stack Developer / Technical Leader</h3>
-                <p><strong>RAYADIAR System (Isfahan, Iran)</strong></p>
-                <p><em>Feb 2014 - Jun 2019</em></p>
-                <ul>
-                  <li>Utilized Oracle ADF to develop four subsystems from inception.</li>
-                  <li>Collaborated with analysis teams to optimize business processes and increase overall efficiency.</li>
-                  <li>Spearheaded the implementation of high-level modules and RestFul, JAX-RS, and JAX-WS APIs to accelerate both development and deployment times.</li>
-                  <li>Implemented a robust payment gateway, seamlessly integrating with APIs from five major banks in Iran for enhanced financial transactions.</li>
-                  <li>Standardized problem-solving methods that improved the efficacy of solutions by 30%.</li>
-                  <li>Constructed a DSS application tailored to the MSC's needs, achieving a cost savings of over 20%.</li>
-                </ul>
-                <p><strong>Stack:</strong> Java 6-8 & J2EE, JSF2, Servlet, Oracle DB, ADF, BPMS, WebLogic, OAM, OID, and OEM, ELK</p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </section>
-
-
-
-
-
-      <section id="education" className="bg-light">
-        <div className="container">
+      <section id="education" className="bg-light ">
+        <div className="row">
           <h2 className="section-title">Education</h2>
           <div className="row">
             <div className="col-md-6">
