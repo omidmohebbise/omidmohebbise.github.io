f71cdf207a80c6e3c9185f8d4a06745fd6ecc644
Omid Mohebbi <omidmohebbise@gmail.com>
2024-12-23 13:26:23 +0100

    fix build issue

--- src/components/ai/header/AiHeader.tsx
+++ src/components/ai/header/AiHeader.tsx
@@ -1,25 +1,27 @@
 import { Badge } from "react-bootstrap";
 
-export enum AiPageHeaders {
+export enum Headers {
     Intro = 'intro',
     Models = 'models',
     Learning = 'learning',
     AITools = 'tools',
 }
 
+interface AiPageHeaderProps {
+    selected: Headers; 
+}
 
-
-const AiPageHeader: React.FC<AiPageHeaders> = ({ selected }) => {
+const AiPageHeader: React.FC<AiPageHeaderProps> = ({ selected }) => {
 
     let cssClass = "mx-1"
     console.log(selected)
     console.log("DDDD")
 
     return <div>
-        <Badge className={cssClass} color={selected===AiPageHeaders.Intro ? "success" : ""}>Introduction</Badge>
-        <Badge className={cssClass} color={selected===AiPageHeaders.Models ? "success" : ""}>Common models</Badge>
-        <Badge className={cssClass} color={selected===AiPageHeaders.Learning ? "success" : ""}>AI Learning</Badge>
-        <Badge className={cssClass} color={selected===AiPageHeaders.AITools ? "success" : ""}>AI Tools</Badge>
+        <Badge className={cssClass} color={selected===Headers.Intro ? "success" : ""}>Introduction</Badge>
+        <Badge className={cssClass} color={selected===Headers.Models ? "success" : ""}>Common models</Badge>
+        <Badge className={cssClass} color={selected===Headers.Learning ? "success" : ""}>AI Learning</Badge>
+        <Badge className={cssClass} color={selected===Headers.AITools ? "success" : ""}>AI Tools</Badge>
 
     </div>;
 };
